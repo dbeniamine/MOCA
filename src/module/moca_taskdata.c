55538abd16eeea626110de60bca8722acec3d9d5
David Beniamine <david.beniamine@imag.fr>
2015-01-23 15:27:09 +0100

    PAGE_SIZE added to task0 output

--- src/module/moca_taskdata.c
+++ src/module/moca_taskdata.c
@@ -386,8 +386,12 @@ static ssize_t Moca_FlushData(struct file *filp,  char *buffer,
         MOCA_DEBUG_PRINT("Moca first flush for data %p\n", data);
 
         //Task id pid
-        sz=snprintf(MYBUFF,MOCA_BUF_SIZE,"Task %d %d\n",
-                data->internalId,task_pid_nr(data->task));
+        if(data->internalId==0)
+            sz=snprintf(MYBUFF,MOCA_BUF_SIZE,"Task %d %d\n",
+                    data->internalId,task_pid_nr(data->task));
+        else
+            sz=snprintf(MYBUFF,MOCA_BUF_SIZE,"Task %d %d %lu\n",
+                    data->internalId,task_pid_nr(data->task), PAGE_SIZE);
         MOCA_DEBUG_PRINT("Moca buf size %lu\n", sz);
         if(!copy_to_user(buffer, MYBUFF,sz))
             len+=sz;
