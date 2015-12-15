626dea1b8a7825696bdff2484b56aeba3e2a2396
David Beniamine <david.beniamine@imag.fr>
2015-12-15 15:25:24 +0100

    event extractor for new format

--- test/events_extractor.sh
+++ test/events_extractor.sh
@@ -33,12 +33,12 @@ do cd $f
             | awk 'BEGIN{s=0}{s+=$1}END{print s}')
         echo "$f,$id,Pin,$pages,$pages,$nacc" >> $WD/events.csv
         # Moca values
-        addr=$(find . -name Moca-task*.log | xargs cat | grep ^A \
-            | awk '{print $2}' | sort -u | wc -l)
-        pages=$(find . -name Moca-task*.log | xargs cat | grep ^A \
-            | awk '{print $2}' | sed 's/\(.*\)[a-f0-9]\{3\}/\1/' | sort -u | wc -l)
-        nacc=$(find . -name Moca-task*.log | xargs cat | grep ^A \
-            | awk 'BEGIN{s=0}{s+=$4+$5}END{print s}')
+        addr=$(find . -name Moca-*.csv | sed '1d' Moca-bt.csv | \
+            cut -d ',' -f 1 | sort -u | wc -l)
+        pages=$(find . -name Moca-*.csv | sed '1d' Moca-bt.csv | \
+            cut -d ',' -f 1 | sed 's/\(.*\)[a-f0-9]\{3\}/\1/' | sort -u | wc -l)
+        nacc=$(find . -name Moca*.csv | cut -d ',' -f 3,4 | tr ',' ' ' \
+            | awk 'BEGIN{s=0}{s+=$1+$2}END{print s}')
         echo "$f,$id,Moca,$pages,$addr,$nacc" >> $WD/events.csv
         cd ..
     done
