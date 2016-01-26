99ecc835935c47d3de20778caf422750e3d529b7
David Beniamine <david.beniamine@imag.fr>
2016-01-26 18:33:01 +0100

    Framesoc files new traces

---
+++ src/utils/create_event_producer.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Copyright (C) 2015  Beniamine, David <David@Beniamine.net>
+# Author: Beniamine, David <David@Beniamine.net>
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+if [ -z "$2" ]
+then
+    echo "Usage $0 PageSize TraceFile"
+    exit 1
+fi
+
+TYPES=('Virtual' 'Physical')
+
+# $1: type
+# $2: pagesize
+# $3: trace file
+generate_producers()
+{
+    field=$(( $1 + 1 ))
+    name=${TYPES[$1]}-producers.log
+    echo $2 > $name
+    sed 1d $3 | cut -d , -f 1,8 | sed 's/...,//' | sort -u |\
+        awk 'BEGIN{CUR=-1} {ADDR=sprintf("%s", $1);if(CUR==ADDR)\
+        {TSKS=TSKS","$2}else{if(CUR!=-1){print CUR","TSKS};CUR=ADDR;TSKS=$2}}\
+            END{print CUR","TSKS}' >> $name
+}
+
+generate_producers 0 $1 $2 &
+pid0=$!
+#generate_producers 1 $1 $2 &
+pid1=$!
+wait $pid0
+wait $pid1
